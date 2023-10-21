b0a1f89d60f0286651360b00cacd9d0ff29a3467
Arthur Padilha <99270875+P4d1lh4@users.noreply.github.com>
2023-10-21 17:12:05 -0300

    Update client.py with adding a timer

--- client.py
+++ client.py
@@ -1,15 +1,17 @@
 import socket as s
+import time
 
 def checksum(data):
     byte_count = len(data)
     print(f"N. bytes: {byte_count}")
+    return
 
 def send_packet(seq_num, data, addr, client_socket):
     packet = str(seq_num).encode() + b"|" + data
     checksum(packet)
     client_socket.sendto(packet, addr)
 
-if __name__ == "__main":
+if __name__ == "__main__":
     HOST = s.gethostbyname(s.gethostname())
     PORT = 8080
     client = s.socket(s.AF_INET, s.SOCK_DGRAM)
@@ -21,4 +23,3 @@ if __name__ == "__main":
         data = data.encode("utf-8")
         send_packet(next_seq_num, data, addr, client)
         next_seq_num += 1
-
