f17dfb40cebb6486b1bf39e08a47a355c48a2e17
Arthur Padilha <99270875+P4d1lh4@users.noreply.github.com>
2023-10-21 17:41:34 -0300

    Update client.py with recognition plus negative recognition

--- client.py
+++ client.py
@@ -3,22 +3,20 @@ import socket as s
 def checksum(data):
     byte_count = len(data)
     print(f"N. bytes: {byte_count}")
+    return
 
-def send_packet(seq_num, data, addr, client_socket):
-    packet = str(seq_num).encode() + b"|" + data
-    checksum(packet)
-    client_socket.sendto(packet, addr)
+def ping(host, port):
+    client = s.socket(s.AF_INET, s.SOCK_DGRAM)
+    addr = (host, port)
+    data = b"ping"
+    client.sendto(data, addr)
+    data, addr = client.recvfrom(1024)
+    if data == b"pong":
+        print(f"{host} is up!")
+    else:
+        print(f"{host} is down!")
 
-if __name__ == "__main":
+if __name__ == "__main__":
     HOST = s.gethostbyname(s.gethostname())
     PORT = 8080
-    client = s.socket(s.AF_INET, s.SOCK_DGRAM)
-    addr = (HOST, PORT)
-    next_seq_num = 0
-
-    while True:
-        data = input("Enter a word: ")
-        data = data.encode("utf-8")
-        send_packet(next_seq_num, data, addr, client)
-        next_seq_num += 1
-
+    ping(HOST, PORT)
