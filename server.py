0ec74e690a1a7654c1e6c874cef0f0070252ad53
Arthur Padilha <99270875+P4d1lh4@users.noreply.github.com>
2023-10-21 17:11:38 -0300

    Update server.py with adding a timer

--- server.py
+++ server.py
@@ -1,14 +1,23 @@
 import socket as s
+import time
 
 def checksum(data):
     byte_count = len(data)
     print(f"N. bytes: {byte_count}")
+    return
+
+def send_packet(seq_num, data, addr, client_socket):
+    packet = str(seq_num).encode() + b"|" + data
+    checksum(packet)
+    client_socket.sendto(packet, addr)
 
 if __name__ == "__main__":
+    
     HOST = s.gethostbyname(s.gethostname())
     PORT = 8080
     server = s.socket(s.AF_INET, s.SOCK_DGRAM)
-    server.bind((HOST, PORT)
+    server.bind((HOST, PORT))
+    server.bind((HOST, PORT))
     expected_seq_num = 0
 
     while True:
@@ -17,6 +26,11 @@ if __name__ == "__main__":
         seq_num = int(seq_num)
         data = data.decode("utf-8")
 
+        print("Client: {data}")
+        data = data.upper()
+        data = data.encode("utf-8")
+        checksum(data)
+        server.sendto(data, addr)
         if seq_num == expected_seq_num:
             print(f"Received from Client: {data}")
             data = data.upper()
