f7821d784c0ea2d3629f3c30ee30dc3ac7787829
FernandoM <89084956+Nand0000000000@users.noreply.github.com>
2023-10-21 16:16:25 -0300

    Update server.py with sequence number

--- server.py
+++ server.py
@@ -3,21 +3,26 @@ import socket as s
 def checksum(data):
     byte_count = len(data)
     print(f"N. bytes: {byte_count}")
-    return
 
 if __name__ == "__main__":
-    
     HOST = s.gethostbyname(s.gethostname())
     PORT = 8080
     server = s.socket(s.AF_INET, s.SOCK_DGRAM)
-    server.bind((HOST, PORT))
+    server.bind((HOST, PORT)
+    expected_seq_num = 0
 
     while True:
         data, addr = server.recvfrom(1024)
+        seq_num, data = data.split(b"|", 1)
+        seq_num = int(seq_num)
         data = data.decode("utf-8")
 
-        print("Client: {data}")
-        data = data.upper()
-        data = data.encode("utf-8")
-        checksum(data)
-        server.sendto(data, addr)
+        if seq_num == expected_seq_num:
+            print(f"Received from Client: {data}")
+            data = data.upper()
+            data = data.encode("utf-8")
+            checksum(data)
+            server.sendto(data, addr)
+            expected_seq_num += 1
+        else:
+            print(f"Received out-of-order packet with seq_num {seq_num}. Ignoring.")
